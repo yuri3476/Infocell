84e9153940a90ad019cad8e498696489e5c5c766
Yuri Rezende <yuri.rezende@souunit.com.br>
2023-02-09 20:35:34 -0300

    Projeto Infocell
    Iniciando criando uma tela exibindo todos produtos de uma API e criando sistema de carrinho

---
+++ src/pages/Store.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { BsCart4, BsFillCartCheckFill } from 'react-icons/bs';
+import { getItem, setItem } from '../services/LocalStorageFuncs'; 
+
+// chamando API
+export const Store = () => {
+
+    const [data, setData] = useState([]);
+    const [cart, setCart] = useState( getItem('carrinhoYt') || []);  // ele vai guardar os itens que estão no carrinho
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            const url = 'https://api.mercadolibre.com/sites/MLB/search?q=celular';
+            const response = await fetch(url);
+            const objJson = await response.json();
+            setData(objJson.results);
+        }
+        fetchApi();
+    }, [])
+
+    // função para adicionar e remover do carrinho
+    const HandleClick = (obj) => {
+        const element = cart.find((e) => e.id === obj.id)
+        if(element){
+            const arrFilter = cart.filter((e) => e.id !== obj.id)
+            setCart(arrFilter)
+            setItem('carrinhoYt', arrFilter)
+        } else{
+            setCart([...cart,obj])
+            setItem('carrinhoYt', [...cart,obj])
+        }
+
+    }
+
+    return(
+        <div>
+            <h1>Store</h1>
+        <div>
+            {
+                // puxando da APi para exibir na tela
+                data.map((e) => (
+                    <div key={e.id}>
+                        <h4>{e.title}</h4>
+                        <img src={e.thumbnail} alt=''/>
+                        <h4>{e.price}</h4>
+                        <button
+                        onClick={() => HandleClick(e)}>
+                            {
+                                cart.some((itemCart) => itemCart.id === e.id) ?(
+                                    <BsFillCartCheckFill/>
+                                ) :
+                                <BsCart4/>
+                            }
+                        </button>
+                    </div>
+                ))
+            }
+            </div>    
+        </div>
+
+    )
+}
---
+++ src/services/LocalStorageFuncs.js
@@ -0,0 +1,9 @@
+
+// funções do local Storage
+export const setItem = (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value))
+}
+
+export const getItem = (key) => {
+    return JSON.parse(localStorage.getItem(key))
+}
